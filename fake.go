5c77ad8d1dbd6da2f40b035b136c81c79227eb99
Florin Patan <florinpatan@gmail.com>
2020-02-25 18:09:23 +0200

    Simplify code

--- fake.go
+++ fake.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"math/rand"
@@ -33,33 +31,35 @@ func fakeTraffic() {
 		i++
 
 		page := pages[rand.Intn(len(pages))]
-		page = strings.Replace(page, "{productID}", "abc-"+strconv.Itoa(int(i)), -1)
-		r, err := http.NewRequest(http.MethodGet, "http://localhost:8080"+page, nil)
-		if err != nil {
-			continue
-		}
-		r = r.WithContext(context.WithValue(context.Background(), "requestID", i))
 
-		go func(i int64) {
-			debugger.Middleware(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Println(r.Context().Value("requestID"))
-				makeRequest(activeConns, c, r)
-			}, func(r *http.Request) []string {
-				return []string{
-					"request", "automated",
-					"page", page,
-					"rid", strconv.FormatInt(i, 10),
-				}
-			})(nil, r)
-		}(i)
+		// We need to launch this using a closure function to
+		// ensure that we capture the correct value for the
+		// two parameters we need: page and i
+		go func(p string, rid int64) {
+			makeRequest(activeConns, c, p, rid)
+		}(page, i)
 	}
 }
 
-func makeRequest(done chan struct{}, c *http.Client, r *http.Request) {
+func makeRequest(done chan struct{}, c *http.Client, page string, i int64) {
 	defer func() {
 		// Unblock the next request from the queue
 		<-done
 	}()
+
+	debugger.SetLabels(func() []string {
+		return []string{
+			"request", "automated",
+			"page", page,
+			"rid", strconv.Itoa(int(i)),
+		}
+	})
+
+	page = strings.Replace(page, "{productID}", "abc-"+strconv.Itoa(int(i)), -1)
+	r, err := http.NewRequest(http.MethodGet, "http://localhost:8080"+page, nil)
+	if err != nil {
+		return
+	}
 
 	resp, err := c.Do(r)
 	if err != nil {
